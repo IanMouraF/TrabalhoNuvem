45faf6f4e254b6b7aa61d8855fabf71d33712ed5
IanMouraF <ianmourafac@gmail.com>
2023-09-11 21:31:16 -0300

    Correção compenentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
 import Header from './components/header'
-import About from './components/About'
-import Carrinho from './components/Cart'
-import Contact from './components/Contact'
+import Carrinho from './components/Carrinho'
 import Footer from './components/Footer'
-import Carrossel from './components/Home'
-import Produtos from './components/Menu'
+import Carrossel from './components/Carrossel'
+import Produtos from './components/Produtos'
 import NavIcon from './components/NavIcon'
 
 export default function App() {
