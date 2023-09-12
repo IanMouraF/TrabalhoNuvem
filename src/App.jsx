ad1ca71e2c3040f062396aeb1a156465f9a3be3a
IanMouraF <ianmourafac@gmail.com>
2023-09-12 15:38:59 -0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Header from './components/Header/Header'
 import Carrinho from './components/Carrinho/Carrinho'
 import Footer from './components/Footer'
-import Carrossel from './components/Carrossel'
+import Carrossel from './components/Carrossel/Carrossel'
 import Produtos from './components/Produtos'
 import NavIcon from './components/NavIcon'
 
