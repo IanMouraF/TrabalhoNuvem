8a75eacf1ad5705faca354c5cf26ac7a0e8977d0
Jonathas Rodrigues <jonathashnr@gmail.com>
2023-09-13 14:40:39 -0300

    Resolve o problema em App.jsx não executa pois o nomo Header no import estava escrito errado

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
-import React from 'react'
-import Header from './components/header'
-import Carrinho from './components/Carrinho'
-import Footer from './components/Footer'
-import Carrossel from './components/Carrossel'
-import Produtos from './components/Produtos'
-import NavIcon from './components/NavIcon'
+import React from "react";
+import Header from "./components/Header";
+import Carrinho from "./components/Carrinho";
+import Footer from "./components/Footer";
+import Carrossel from "./components/Carrossel";
+import Produtos from "./components/Produtos";
+import NavIcon from "./components/NavIcon";
 
 export default function App() {
-  return (
-    <div>
-      <Header />
-      <NavIcon />
-      <Carrossel />
-      <Produtos />
-      <Carrinho />
-      <Footer />
-    </div>
-  )
+    return (
+        <div>
+            <Header />
+            <NavIcon />
+            <Carrossel />
+            <Produtos />
+            <Carrinho />
+            <Footer />
+        </div>
+    );
 }
