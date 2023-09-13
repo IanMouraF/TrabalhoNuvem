eeee0bfaa0ab1778e1639085a8b658a10612f5a7
Jonathas Rodrigues <jonathashnr@gmail.com>
2023-09-13 19:51:15 -0300

    Fix no App.jsx para rodar todos os componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,23 @@
-import React from 'react'
-import Header from './components/Header/Header'
-import Carrinho from './components/Carrinho/Carrinho'
-import Carrossel from './components/Carrossel/Carrossel'
-import Produtos from './components/Produtos'
-import NavIcon from './components/NavIcon'
-import Footer from './components/Footer/Footer'
-import './App.css'
+import React from "react";
+import Header from "./components/Header/Header";
+import Carrinho from "./components/Carrinho/Carrinho";
+import Carrossel from "./components/Carrossel/Carrossel";
+import Produtos from "./components/Produtos/Produtos";
+import NavIcon from "./components/NavIcon";
+import Footer from "./components/Footer/Footer";
+import "./App.css";
 
 export default function App() {
-  return (
-    <div>
-      <Header/>
-      <Carrinho />
-      <Footer/>
-    </div>
-  )
     return (
         <div>
+            <Header />
             <Produtos />
             <div className="ian">
                 <Carrossel />
                 <div className="teste"></div>
                 <Carrinho />
             </div>
+            <Footer />
         </div>
     );
 }
