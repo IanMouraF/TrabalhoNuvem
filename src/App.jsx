3198024e2a06bfea216acf454edc77bc8931f28d
Junior Souza <juniorsouza@Juniors-MacBook-Pro.local>
2023-09-13 15:31:18 -0300

    Carousel compoment

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import Header from './components/Header/Header'
 import Carrinho from './components/Carrinho/Carrinho'
 import Footer from './components/Footer'
-import Carrossel from './components/Carrossel'
+import Carrossel from './components/Carrossel/Carrossel.jsx'
 import Produtos from './components/Produtos'
 import NavIcon from './components/NavIcon'
 
 export default function App() {
   return (
     <div>
+      <Carrossel />
       <Carrinho />
     </div>
   )
