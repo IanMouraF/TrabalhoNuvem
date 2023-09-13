0c845f7b62f7e9aa9dbc4ebdd9fb32b9ccb17415
Jonathas Rodrigues <jonathashnr@gmail.com>
2023-09-13 16:33:59 -0300

    Adiciona estilos basicos as informações do produto

--- src/components/Produtos.jsx
+++ src/components/Produtos.jsx
@@ -3,15 +3,15 @@ import "./ProdutosStyles.css";
 const Produtos = () => {
     // produtos pra testar a funcionalidade
     const products = [
-        { id: 1, price: 19.99, name: "Produto 1" },
-        { id: 2, price: 4.99, name: "Produto 2" },
-        { id: 3, price: 9.99, name: "Produto 3" },
-        { id: 4, price: 1.99, name: "Produto 4" },
-        { id: 5, price: 29.99, name: "Produto 5" },
-        { id: 6, price: 12.99, name: "Produto 6" },
-        { id: 7, price: 7.99, name: "Produto 7" },
-        { id: 8, price: 4.99, name: "Produto 8" },
-        { id: 9, price: 9.99, name: "Produto 9" },
+        { id: 1, price: 19.99, description: "bla bla bla", name: "Produto 1" },
+        { id: 2, price: 4.99, description: "bla bla bla", name: "Produto 2" },
+        { id: 3, price: 9.99, description: "bla bla bla", name: "Produto 3" },
+        { id: 4, price: 1.99, description: "bla bla bla", name: "Produto 4" },
+        { id: 5, price: 29.99, description: "bla bla bla", name: "Produto 5" },
+        { id: 6, price: 12.99, description: "bla bla bla", name: "Produto 6" },
+        { id: 7, price: 7.99, description: "bla bla bla", name: "Produto 7" },
+        { id: 8, price: 4.99, description: "bla bla bla", name: "Produto 8" },
+        { id: 9, price: 9.99, description: "bla bla bla", name: "Produto 9" },
     ];
     return (
         <div className="products-component">
@@ -20,8 +20,14 @@ const Produtos = () => {
                 {products.map((product) => (
                     <div className="product" key={product.id}>
                         <div className="product-image"></div>
-                        <div>
-                            {product.name}, R$ {product.price}
+                        <div className="product-info">
+                            <div className="product-name">{product.name}</div>
+                            <div className="product-price">
+                                R$ {product.price}
+                            </div>
+                            <div className="product-description">
+                                {product.description}
+                            </div>
                         </div>
                     </div>
                 ))}
