87a7170fafa4932e67e13308cb92af5e11218f96
samuelVRcoder <118441330+samuelVRcoder@users.noreply.github.com>
2023-09-24 13:13:31 -0300

    Update Logout.js
    corrigindo erro de path do firebase.js

--- src/Logout.js
+++ src/Logout.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {  signOut } from "firebase/auth";
-import {auth} from './firebase_setup/firebase';
+import {auth} from './firebase';
 import { useNavigate } from 'react-router-dom';
  
 const Logout = () => {
