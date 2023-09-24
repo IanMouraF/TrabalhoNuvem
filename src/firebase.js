85514d6665823f1cbef4c0b7d381d5c89346b9a7
samuelVRcoder <118441330+samuelVRcoder@users.noreply.github.com>
2023-09-24 13:21:11 -0300

    Update firebase.js
    add auth para trabalhar c/ logout

--- src/firebase.js
+++ src/firebase.js
@@ -2,6 +2,7 @@
 
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore/lite";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
     apiKey: "AIzaSyBp9xsaRvnhmE5sDuEviUR9nqGHxXAQFhY",
@@ -17,5 +18,7 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const firestore = getFirestore(app);
+
+export const auth = getAuth(app);
 
 export { firestore };
