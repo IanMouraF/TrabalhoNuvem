6713a9213f199bda13cd084178cdad28a160cb1f
Junior Souza <juniorsouza@Juniors-MacBook-Pro.local>
2023-09-27 22:03:06 -0300

    estilo modal criaçaoç

---
+++ src/components/ModalGerenciamento/ModalGerenciamento.jsx
@@ -0,0 +1,58 @@
+import '../ModalGerenciamento/ModalGerenciamento.css'
+import CloseIcon from '@mui/icons-material/Close';
+
+const ModalGerenciamento = () => {
+    return (
+        <div className="container-gerenciamento">
+            <div className="fechar">
+                <a href="/">
+                    <CloseIcon/> 
+                </a>
+            </div>
+          
+            <div>
+                <h3>Nome</h3>
+                <input type='string' placeholder='Digite o nome do produto'></input>
+            </div>
+            <div>
+                <h3>Valor</h3>
+                <input type='number' placeholder='Digite o valor do produto'></input>
+            </div>
+            <div>
+                <h3>Descrição</h3>
+                <input className='descricao' type='string' placeholder='Digite uma descrição pro seu produto'></input>
+            </div>
+            <div>
+                <h3>URL Imagem</h3>
+                <input type='string' placeholder='Cole a URL da imagem do seu produto'></input>
+            </div>
+            <div>
+                <h3>Categoria</h3>
+                <input type='string' placeholder='Cole a URL da imagem do seu produto'></input>
+            </div>
+
+            <div className="botoes">
+                <button className='edit'>Editar</button>
+                <button className='delete'>Excluir</button>
+
+            </div>
+        
+            {/* <FormControl fullWidth>
+                <InputLabel id="demo-simple-select-label">Age</InputLabel>
+                <Select
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                    value={age}
+                    label="Age"
+                    onChange={handleChange}
+                >
+                <MenuItem value={10}>Ten</MenuItem>
+                <MenuItem value={20}>Twenty</MenuItem>
+                <MenuItem value={30}>Thirty</MenuItem>
+            </Select>
+            </FormControl> */}
+        </div>
+     )
+}
+
+export default ModalGerenciamento;
